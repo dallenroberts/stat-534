39bb34f08b1e1341b09490e2934da5ac7f6f5c9f
Allen Roberts <dallenr@uw.edu>
2020-05-14 14:04:33 -0700

    a

--- hw5/MatricesGSL/matrices.cpp
+++ hw5/MatricesGSL/matrices.cpp
@@ -153,18 +153,18 @@ double marglik(gsl_matrix* data,int lenA,int* A)
 	int p = data -> size2;
 
 	// Initialize vector corresponding to rows needed for MakeSubmatrix
-	int * rows = new int[n];
+	int * rows = int[n];
 	for(i=0;i<n;i++) {
 
 		rows[i]=i;
 	}
 
 	// Initialize vector corresponding to columns needed for MakeSubmatrix for d1
-	int * cols1 = new int[1];
+	int * cols1 = int[1];
 	cols1[0] = 0;
 
 	// Initialize vector corresponding to columns needed for MakeSubmatrix for dA
-	int * colsA = new int[lenA];
+	int * colsA = int[lenA];
 	for(j=0;j<lenA;j++) {
 
 		colsA[j] = A[j]-1; // C indexes at 0
@@ -180,6 +180,9 @@ double marglik(gsl_matrix* data,int lenA,int* A)
 	gsl_matrix* dA = gsl_matrix_alloc(lenA, n);
 	dA = MakeSubmatrix(data, rows, n, colsA, lenA);
 	printmatrix("dA.mat",dA);
+
+	// Make mA
+
 
 	// lml = lgamma((n + lenA + 2.0)/2.0) - lgamma((lenA + 2.0)/2.0) - (1.0/2.0)*logdet(mA);
 	lml = lgamma((n + lenA + 2.0)/2.0) - lgamma((lenA + 2.0)/2.0);
