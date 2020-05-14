be32a209504d7a594f8d03a3e37473ba744fd74d
Allen Roberts <dallenr@uw.edu>
2020-05-13 23:16:17 -0700

    debug

--- hw5/Matrices/matrices.cpp
+++ hw5/Matrices/matrices.cpp
@@ -301,9 +301,17 @@ double marglik(int n,int p,double** data,int lenA,int* A)
 	printmatrix("mA.mat", lenA, lenA, mA);
 
 	// Calculate lml
+	double** td1 = transposematrix(n, 1, d1);
+
 	double** td1_d1 = allocmatrix(1, 1);
-	matrixproduct(1, n, 1, transposematrix(n, 1, d1), d1, td1_d1);
+	matrixproduct(1, n, 1, td1, d1, td1_d1);
 	printmatrix("td1_d1.mat", 1, 1, td1_d1);
+
+	double** td1_dA = allocmatrix(1, lenA);
+	matrixproduct(1, n, lenA, td1, dA, td1_dA);
+	printmatrix("td1_dA.mat", 1, 1, td1_dA);
+
+	matrixproduct()
 
 
 	lml = lgamma((n + lenA + 2.0)/2.0) - lgamma((lenA + 2.0)/2.0) - (1.0/2.0)*logdet(lenA, mA) - ((n + lenA + 2.0)/2.0);
