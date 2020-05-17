cd101d7d74f5c93e17b4a0ed6c95fa29167decd5
Allen Roberts <dallenr@uw.edu>
2020-05-17 14:08:01 -0700

    a

--- hw6/problem3/matrices.h
+++ hw6/problem3/matrices.h
@@ -3,9 +3,19 @@
 #include <string.h>
 #include <stdlib.h>
 
-extern "C" {
-#include <clapack.h>
-}
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_statistics.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_cdf.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_permutation.h>
+#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_sort_double.h>
+#include <gsl/gsl_sort_vector.h>
+#include <gsl/gsl_errno.h>
 
 extern "C" {
  void dpotri_(char*,int*,double*,int*,int*);
@@ -27,4 +37,12 @@ double logdet(int p,double** m);
 
 //functions related to HW2
 double** submatrix(int n,int p,double** data,int lenA,int* A);
-double marglik(int n,int p,double** data,int lenA,int* A);
+double marglik(gsl_matrix* data, int lenA, int* A);
+
+//
+void subset_gsl_matrix(gsl_matrix* fulldata, gsl_matrix* subdata,
+		int n, int* A, int lenA);
+gsl_matrix* create_MA(gsl_matrix* D_A);
+
+void subset_gsl_matrix(gsl_matrix* fulldata, gsl_matrix* subdata,
+		int n, int* A, int lenA)
