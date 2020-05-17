b5c7583fba56b7b345958e1a7468cc23947ee342
Allen Roberts <dallenr@uw.edu>
2020-05-16 19:20:10 -0700

    a

--- hw6/problem1/main.cpp
+++ hw6/problem1/main.cpp
@@ -1,16 +1,51 @@
 #include "matrices.h"
 
-double getDeterminant(int nrow)
+// Function to return the minor of a matrix for which we want to calculate the determinant
+// Omits the first row and the kth column
+// Input: gsl_matrix * m = nxn matrix; int n = number of rows and columns for m; 
+// int k: index of column to omit, ranges from 0 to n-1
+gsl matrix * getMinor(gsl_matrix * m, int n, int k) {
+
+	int i,j;
+
+	gsl_matrix * minor = gsl_matrix_alloc(n-1, n-1);
+
+	for(i=0;i<(n-1);i++) {
+
+		printf("\ni = %d", i);
+
+
+		for(j=0; j<(n-1);j++) {
+
+			printf("\nj = %d", j);
+
+			if(j<k){
+
+				gsl_matrix_set(minor, i, j, gsl_matrix_get(m, i+1, j));
+
+			} else {
+
+				gsl_matrix_set(minor, i, j, gsl_matrix_get(m, i+1, j+1));
+
+			}
+		}
+	}
+
+	return(minor);
+
+}
+
+double getDeterminant(int n)
 {
 
 	int n;
 	double det;
-	int i,j,k;
+	int k;
 
 	printf("Hello (matrix) world.\n");
 
 	// Read in banded matrix
-	gsl_matrix * m = gsl_matrix_alloc(nrow, nrow);
+	gsl_matrix * m = gsl_matrix_alloc(n, n);
 	FILE * f = fopen("mybandedmatrix.txt", "r");
 	gsl_matrix_fscanf(f, m);
 	fclose(f);
@@ -31,28 +66,9 @@ double getDeterminant(int nrow)
 
 	// Define minor matrix by removing the first row and the kth column (where k ranges from 0 to n-1) from an nxn matrix m
 	k = 3;
-	gsl_matrix * minor = gsl_matrix_alloc(nrow-1, nrow-1);
 
-	for(i=0;i<(nrow-1);i++) {
-
-		printf("\ni = %d", i);
-
-
-		for(j=0; j<(nrow-1);j++) {
-
-			printf("\nj = %d", j);
-
-			if(j<k){
-
-				gsl_matrix_set(minor, i, j, gsl_matrix_get(m, i+1, j));
-
-			} else {
-
-				gsl_matrix_set(minor, i, j, gsl_matrix_get(m, i+1, j+1));
-
-			}
-		}
-	}
+	minor = getMinor(m, n, k);
+	
 
 	printmatrix("minor.mat", minor);
 
