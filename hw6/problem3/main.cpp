fa5812c8ffbb8863b7750c82c567d4d679c5396a
Allen Roberts <dallenr@uw.edu>
2020-05-19 11:31:27 -0700

    a

--- hw6/problem3/main.cpp
+++ hw6/problem3/main.cpp
@@ -39,32 +39,33 @@ int main()
   int A[p-1]; //indices of the variables present in the regression
   int lenA = -1; //number of indices
 
-  // for(i = 1; i<p; i++) {
-  //   //add the regressions with one predictor
-  //   lenA = 1;
-  //   A[0] = i+1;
-  //   AddRegression(nMaxRegs, regressions,
-  //     lenA, A, 
-  //     marglik(n,p,data,lenA,(int*)A));
-
-  //   for(j = 1; j<p; j++) {
-  //     // add the regressions with two predictors
-  //     if(i != j) {
-  //       lenA = 2;
-  //       A[1] = j+1;
-  //       AddRegression(nMaxRegs, regressions,
-  //         lenA, A, 
-  //         marglik(n,p,data,lenA,(int*)A));
-  //     }
-  //   }
-  // }
-
-    lenA = 2;
-    A[0] = 2;
-    A[1] = 24;
+  for(i = 1; i<p; i++) {
+    //add the regressions with one predictor
+    lenA = 1;
+    A[0] = i+1;
     AddRegression(nMaxRegs, regressions,
       lenA, A, 
       marglik(n,p,data,lenA,(int*)A));
+
+    for(j = 1; j<p; j++) {
+      // add the regressions with two predictors
+      if(i != j) {
+        lenA = 2;
+        A[1] = j+1;
+        AddRegression(nMaxRegs, regressions,
+          lenA, A, 
+          marglik(n,p,data,lenA,(int*)A));
+      }
+    }
+  }
+
+    // For testing purposes
+    // lenA = 2;
+    // A[0] = 2;
+    // A[1] = 24;
+    // AddRegression(nMaxRegs, regressions,
+    //   lenA, A, 
+    //   marglik(n,p,data,lenA,(int*)A));
 
   //save the list in a file
   SaveRegressions(outputfilename,regressions);
