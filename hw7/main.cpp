122cedfc0f5d0f1686c92e0bbfbf1130a13699c0
Allen Roberts <dallenr@uw.edu>
2020-05-26 13:08:05 -0700

    a

--- hw7/main.cpp
+++ hw7/main.cpp
@@ -5,12 +5,14 @@
 
 #include <stdio.h>
 #include <gsl/gsl_rng.h>
+#include "matrices.h"
 
 // Function that inputs a pointer to a pxp GSL matrix covX and a pointer to a 
 // nxp data matrix X and calculates the pxp sample covariance matrix sigma
 // Outputs nothing, but updates the gsl_matrix stored at location covX
 void makeCovariance(gsl_matrix* covX, gsl_matrix* X) {
 
+	printf("\n Inside makeCovariance");
 	return(1)
 
 }
@@ -19,7 +21,8 @@ void makeCovariance(gsl_matrix* covX, gsl_matrix* X) {
 int main() {
 
 	int i;
-  	int n = 1000;
+  	int n = 158;
+  	int p = 51;
 
 	// Initialize random number generator
   	const gsl_rng_type* T;
@@ -29,14 +32,24 @@ int main() {
 	
   	T = gsl_rng_default;
   	r = gsl_rng_alloc(T);
+
+  	// Load erdata
+  	gsl_matrix * m = gsl_matrix_alloc(n, p);
+	FILE * f = fopen("erdata.txt", "r");
+	gsl_matrix_fscanf(f, m);
+	fclose(f);
+
+	printmatrix("datamat.mat",m);
 	
-	
+
   	// for(i=0;i<n;i++)
   	// {
   	//   double u = gsl_rng_uniform(r);
   	//   printf("%.5lf\n",u);
   	// }
-	
+
+	// Free memory
+	gsl_matrix_free(m);
   	gsl_rng_free(r);
 	
   	return(1);
