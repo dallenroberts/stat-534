04c92df1afa951c692a874e6a99daf88a80c4d4e
Allen Roberts <dallenr@uw.edu>
2020-05-26 15:34:32 -0700

    a

--- hw7/main.cpp
+++ hw7/main.cpp
@@ -76,7 +76,8 @@ void randomMVN(gsl_rng* mystream, gsl_matrix* samples,gsl_matrix* sigma) {
 
 	// Calculate matrix product X = psi*Z
 	gsl_matrix* X = gsl_matrix_alloc(sigma->size2, samples->size1);
-	gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, psi, z, 0.0, X);
+
+	gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, z, psi, 0.0, X);
 	printmatrix("X.mat",X);
 
 	// Free memory
