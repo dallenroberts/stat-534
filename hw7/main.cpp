3e74c9872267da3dba61c07ff09c46a2140e129c
Allen Roberts <dallenr@uw.edu>
2020-05-26 16:04:00 -0700

    a

--- hw7/main.cpp
+++ hw7/main.cpp
@@ -81,8 +81,11 @@ void randomMVN(gsl_rng* mystream, gsl_matrix* samples,gsl_matrix* sigma) {
 		gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, psi, z, 0.0, X);
 
 		// Store in samples matrix
+		printf("\n i: %d", i);
 		gsl_matrix_get_col(s, X, 0);
+		printf("\n Got col \n")
 		gsl_matrix_set_row(samples, i, s);
+		printf("\n Set row \n\n");
 
 	}
 
