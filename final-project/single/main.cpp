25d144b5b78538da99baf79082164635e43f66b9
Allen Roberts <dallenr@uw.edu>
2020-06-01 20:18:02 -0700

    a

--- final-project/single/main.cpp
+++ final-project/single/main.cpp
@@ -165,13 +165,17 @@ double logisticLogLikStar(int n, gsl_matrix* y, gsl_matrix* x, gsl_matrix* beta)
 		logLik += yi*log(pi) + (1-yi)*log(1-pi);
 	}
 
-	// Calculate sum of squared entries in beta matrix
-	// for(i=0;i<beta->size1;i++) {
-	// 	for(j=0;j<beta->size2;j++) {
+	Calculate sum of squared entries in beta matrix
+	for(i=0;i<(beta->size1);i++) {
+		for(j=0;j<(beta->size2);j++) {
 
-	// 		bsum += pow(gsl_matrix_get(beta, i, j), 2);
-	// 	}
-	// }
+			printf("\n i=%d", i);
+			printf("\n j=%d", j);
+			printf("\n beta[i,j]=%d", gsl_matrix_get(beta, i, j));
+
+			bsum += pow(gsl_matrix_get(beta, i, j), 2);
+		}
+	}
 
 	// printf("\n bsum=%d", bsum);
 
