7bad4507b7f15b5ec6c968773dfb3d42e9ba5a43
Allen Roberts <dallenr@uw.edu>
2020-06-03 16:54:48 -0700

    a

--- final-project/parallel/parallelBayesLogistic.cpp
+++ final-project/parallel/parallelBayesLogistic.cpp
@@ -19,6 +19,10 @@ Final Project
 #include <math.h>
 #include <mpi.h>
 #include <iomanip>
+#include <gsl/gsl_rng.h>
+#include "matrices.h"
+#include "regmodels.h"
+#include "bayes.h"
 
 // For MPI communication
 #define GETR2	1
@@ -380,3 +384,69 @@ void NormStand()
    return;
 }
 
+// Adds a regression using predictor index to the LPRegression list regressions
+void bayesLogistic(int index, int n, int p, int response, gsl_rng* mystream, LPRegression regressions) {
+
+   int i;
+   double lml_la;
+   double lml_mc;
+   int nMaxRegs = 5; // Maximum number of regressions to keep track of
+   int A[p-1]; //indices of the variables present in the regression
+   int lenA = -1; //number of indices
+
+   // Loads 534finalprojectdata.txt. This file has 148 rows (samples) and 61 columns (variables). 
+   // The first 60 columns are associated with 60 explanatory variables X, 
+   // while column 61 (the last column) corresponds with the response binary variable Y
+   gsl_matrix* data = gsl_matrix_alloc(n, p);
+   FILE * f = fopen("534finalprojectdata.txt", "r");
+   gsl_matrix_fscanf(f, data);
+   fclose(f);
+
+   // Initialize predictor and response columns
+   gsl_matrix* x = gsl_matrix_alloc(n, 1);
+   gsl_matrix* y = gsl_matrix_alloc(n, 1);
+   for(i=0;i<n;i++) {
+
+      gsl_matrix_set(x, i, 0, gsl_matrix_get(data, i, index));
+      gsl_matrix_set(y, i, 0, gsl_matrix_get(data, i, response));
+
+   }
+
+   // Calculate beta modes using Newton-Raphson algorithm
+   gsl_matrix* betaMode = getcoefNR(n, y, x, 1000);
+   // printmatrix("betaMode.txt", betaMode);
+
+   // Calculate posterior means for betas
+   gsl_matrix* sampleMeans = getPosteriorMeans(mystream, n, y, x, betaMode, 1000);
+
+   printf(" Sample means:\n");
+   for(i=0;i<(sampleMeans->size1);i++) {
+
+      printf("    beta%i = %.3f\n", i, gsl_matrix_get(sampleMeans, i, 0));
+   }
+   // printmatrix("sampleMeans.txt", sampleMeans);
+
+   // Calculate log marginal likelihood using LaPlace approximation
+   printf("\n Posterior log marginal likelihood P(D) estimates:\n");
+   lml_la = getLaplaceApprox(n, y, x, betaMode);
+   printf("    Laplace approximation = %.3f \n", lml_la);
+
+   // Calculate log marginal likelihood using Monte Carlo integration
+   lml_mc = log(getMC(mystream, n, y, x, 10000));
+   printf("    Monte Carlo integration = %.3f \n", lml_mc);
+
+   // Add to linked list
+   lenA = 1;
+    A[0] = index+1;
+    AddRegression(nMaxRegs, regressions,
+      lenA, A, sampleMeans, lml_la,
+      lml_mc);
+    
+    // Free memory
+   gsl_matrix_free(x);
+   gsl_matrix_free(y);
+   gsl_matrix_free(betaMode);
+   gsl_matrix_free(sampleMeans);
+   gsl_matrix_free(data);
+
+}
