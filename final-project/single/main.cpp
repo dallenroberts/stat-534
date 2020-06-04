b4d606f8f441211189803af33dedecbc0f059f52
Allen Roberts <dallenr@uw.edu>
2020-06-04 14:22:48 -0700

    a

--- final-project/single/main.cpp
+++ final-project/single/main.cpp
@@ -42,7 +42,7 @@ void bayesLogistic(int index, int n, int p, int response, gsl_rng* mystream, LPR
 	// printmatrix("betaMode.txt", betaMode);
 
 	// Calculate posterior means for betas
-	gsl_matrix* sampleMeans = getPosteriorMeans(mystream, n, y, x, betaMode, 1000);
+	gsl_matrix* sampleMeans = getPosteriorMeans(mystream, n, y, x, betaMode, 10000);
 
 	printf(" Sample means:\n");
 	for(i=0;i<(sampleMeans->size1);i++) {
@@ -57,7 +57,7 @@ void bayesLogistic(int index, int n, int p, int response, gsl_rng* mystream, LPR
 	printf("    Laplace approximation = %.3f \n", lml_la);
 
 	// Calculate log marginal likelihood using Monte Carlo integration
-	lml_mc = log(getMC(mystream, n, y, x, 10000));
+	lml_mc = log(getMC(mystream, n, y, x, 100000));
 	printf("    Monte Carlo integration = %.3f \n", lml_mc);
 
 	// Add to linked list
